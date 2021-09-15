f4f9fbe991e70e74320c01a22aaa85ebd526d373
TulaMagar <36879268+IshkMagar@users.noreply.github.com>
2021-09-15 12:26:09 -0400

    excel scanner

--- excelsecurity/settings.py
+++ excelsecurity/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
